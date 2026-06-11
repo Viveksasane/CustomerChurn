9c59a9e899807a888634aadab6cf68667a511f17
Viveksasne <viveksasane9978@gmail.com>
2026-06-11 19:57:56 +0530

    requirements added

---
+++ main.py
@@ -0,0 +1,1 @@
+from src.project.config.configuration import main
--- setup.py
+++ setup.py
@@ -6,6 +6,24 @@ with open("README.md","r",encoding="utf-8") as f:
 
 __version__="0.0.0"
 
-REPO_NAME="Customer_Churn"
-AUTHOR_USER_NAME="Vivek Sasane"
-SRC_REPO=""
+REPO_NAME="CustomerChurn"
+AUTHOR_USER_NAME="Viveksasane"
+SRC_REPO="project"
+AUTHOR_EMAIL="viveksasane9978@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Customer Churn project",
+    long_description=long_description,
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/",
+    project_urls={
+        "Bug Tracker":f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
+
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
